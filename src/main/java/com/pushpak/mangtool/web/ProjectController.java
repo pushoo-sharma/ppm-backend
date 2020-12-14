1513ad8541c4c50aa99e38e6d22c70cedfb89753
Pushpak Sharma <sharmatryhard@gmail.com>
2020-12-14 14:33:46 +0530

    Binding Result commit: Task 2

--- src/main/java/com/pushpak/mangtool/web/ProjectController.java
+++ src/main/java/com/pushpak/mangtool/web/ProjectController.java
@@ -6,7 +6,10 @@ import com.pushpak.mangtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/project")
@@ -19,7 +22,10 @@ public class ProjectController {
 
 
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if(errorMap!=null) return errorMap;
+
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
     }
